3bbab910f932e445614292652a30bf35261a92f6
AnteKolo <ante.kolobaric@yahoo.com>
2017-05-04 09:55:14 +0300

    Add files via upload

---
+++ JOJO3.js
@@ -0,0 +1,6 @@
+function gum() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    document.getElementById("s").innerHTML = "Sirina prozora je " + w + ".";
+    document.getElementById("v").innerHTML = "Visina prozora je " + h + ".";
+}
